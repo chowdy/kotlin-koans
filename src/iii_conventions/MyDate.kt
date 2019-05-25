a14b06b2fd2bebad60602068552e38e57938b65a
Charlie Copprell <charles.copprell@workday.com>
2019-05-24 17:13:55 -0700

    going home
    Change-Id: I7019eb2d9f6a7fc5f623592d329ab6ba0eaf3d79

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,8 +1,19 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        return this.toNum() - other.toNum()
+    }
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+    fun toNum() : Int = dayOfMonth + month * 100 + year * 10000
+
+    //operator fun plus(interval: TimeInterval) : MyDate {
+    //}
+}
+
+operator fun MyDate.rangeTo(other: MyDate): DateRange {
+    return DateRange(this, other)
+}
 
 enum class TimeInterval {
     DAY,
@@ -10,4 +21,24 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class DateRange(val start: MyDate, val endInclusive: MyDate) : Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> {
+        return object: Iterator<MyDate> {
+            var current: MyDate = start
+
+            override fun hasNext(): Boolean {
+                return current <= endInclusive
+            }
+
+            override fun next(): MyDate {
+                val next = current
+                current = current.nextDay()
+                return next
+            }
+        }
+    }
+
+    operator fun contains(d: MyDate) : Boolean {
+        return d >= start && d <= endInclusive
+    }
+}
